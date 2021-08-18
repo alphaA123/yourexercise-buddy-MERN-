dc0d5f163a750ffab53566f93741cc4c131b965b
Abhishek Kumar <abhishekkgodda@gmail.com>
2021-08-18 15:36:13 +0530

    level 2 checkpoint before axios 1.20

---
+++ src/components/edit-exercise.component.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class EditExercises extends Component {
+  render() {
+    return (
+      <div>
+        <p>You are on Edit Exercises component</p>
+      </div>
+    );
+  }
+}
+
+export default EditExercises;
